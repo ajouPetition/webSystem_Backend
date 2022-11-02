b86019624e04ce618ff7eb82eaf40744937608cc
joon2301 <joon2301@naver.com>
2022-11-02 22:42:32 +0900

    comments api 추가

---
+++ appAPI/routes/comments.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+const comments = require("../controller/comments.controller")
+
+// 도메인/api/comments/ 주소
+router.get('/view/:postID', comments.findByPostID);
+router.post('/upload',comments.create)
+router.put('/modify',comments.update)
+router.delete('/delete/:commentID',comments.delete)
+
+module.exports = router;
