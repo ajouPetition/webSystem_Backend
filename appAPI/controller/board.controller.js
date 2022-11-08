24107297dc4b8e6c33cf032b30b5aba6803420c0
joon2301 <joon2301@naver.com>
2022-11-08 16:43:03 +0900

    axios 통신용 이름변경

--- appAPI/controller/board.controller.js
+++ appAPI/controller/board.controller.js
@@ -11,7 +11,7 @@ exports.viewAll = function(req,res){
 
 // 게시물 필터링 조회
 exports.filter = function(req,res){
-    board.filter(req.query, function(err,result){
+    board.filter(req.body, function(err,result){
         if(err) res.send(err)
         res.json(result)
     })
@@ -27,17 +27,17 @@ exports.findByID = function(req,res){
 
 // 게시물 작성
 exports.create = function(req,res){
-    console.log(req.query)
+    console.log(req.body)
     const today = new Date()
-    req.query.date = today.getFullYear().toString() + '-' + (today.getMonth()+1).toString() + '-' + today.getDate().toString() +' 9:00'
-    console.log(req.query.date)
+    req.body.date = today.getFullYear().toString() + '-' + (today.getMonth()+1).toString() + '-' + today.getDate().toString() +' 9:00'
+    console.log(req.body.date)
     console.log()
     let sql = 'SELECT max(postID) as ID from board'
     conn.query(sql,(err, row, fields) => {
 		console.log("error: ", err);
 		console.log("입력될 ID: ", row[0]['ID']+1);
-        req.query.postID = row[0]['ID']+1
-        board.create(req.query, function(err,result){
+        req.body.postID = row[0]['ID']+1
+        board.create(req.body, function(err,result){
             if(err) res.send(err)
             res.json(result)
         })
@@ -46,8 +46,8 @@ exports.create = function(req,res){
 
 // 게시물 수정 <- 청원 내용이 수정될 가능성인데 추가할지 모르겠음
 exports.update = function(req,res){
-    console.log(req.query)
-    board.update(req.query, function(err, result){
+    console.log(req.body)
+    board.update(req.body, function(err, result){
         if(err) res.send(err)
         res.json(result)
     })
--- appAPI/controller/users.controller.js
+++ appAPI/controller/users.controller.js
@@ -11,13 +11,13 @@ exports.findByID = function(req,res){
 
 // 유저 생성
 exports.create = function(req,res){
-    console.log(req.query)
+    console.log(req.body)
     let sql = 'SELECT max(userID) as ID from users'
     conn.query(sql,(err, row, fields) => {
 		console.log("error: ", err);
 		console.log("입력될 ID: ", row[0]['ID']+1);
-        req.query.userID = row[0]['ID']+1
-        users.create(req.query, function(err,result){
+        req.body.userID = row[0]['ID']+1
+        users.create(req.body, function(err,result){
             if(err) res.send(err)
             res.json(result)
         })
@@ -26,7 +26,7 @@ exports.create = function(req,res){
 
 // 유저 정보 수정
 exports.update = function(req,res){
-    users.update(req.query, function(err,result){
+    users.update(req.body, function(err,result){
         if(err) res.send(err)
         res.json(result)
     })
