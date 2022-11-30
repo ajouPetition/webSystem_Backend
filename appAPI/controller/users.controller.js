87fb8d0e139246e9e339262251163a73c3be4cce
Sungpyo <pwsusc10@naver.com>
2022-11-30 14:53:27 +0900

    add jwt auth

--- appAPI/controller/users.controller.js
+++ appAPI/controller/users.controller.js
@@ -1,49 +1,49 @@
-const users = require("../models/users.model")
+const users = require("../models/users.model");
 const conn = require("../config/db");
 
 //로그인
-exports.login = function(req,res){
-    users.login(req.body, function(err, result){
-        if(err) res.send(err)
-        res.json(result)
-    })
-}
+exports.login = function (req, res) {
+  users.login(req.body, function (err, result) {
+    if (err) res.send(err);
+    res.json(result);
+  });
+};
 
 // ID 조회
-exports.findByID = function(req,res){
-    users.findByID(req.params.userID, function(err,result){
-        if(err) res.send(err)
-        res.json(result)
-    })
-}
+exports.findByID = function (req, res) {
+  users.findByID(req.params.userID, function (err, result) {
+    if (err) res.send(err);
+    res.json(result);
+  });
+};
 
 // 유저 생성
-exports.create = function(req,res){
-    console.log(req.body)
-    let sql = 'SELECT max(userID) as ID from users'
-    conn.query(sql,(err, row, fields) => {
-		console.log("error: ", err);
-		console.log("입력될 ID: ", row[0]['ID']+1);
-        req.body.userID = row[0]['ID']+1
-        users.create(req.body, function(err,result){
-            if(err) res.send(err)
-            res.json(result)
-        })
-	});
-}
+exports.create = function (req, res) {
+  console.log(req.body);
+  let sql = "SELECT max(userID) as ID from users";
+  conn.query(sql, (err, row, fields) => {
+    console.log("error: ", err);
+    console.log("입력될 ID: ", row[0]["ID"] + 1);
+    req.body.userID = row[0]["ID"] + 1;
+    users.create(req.body, function (err, result) {
+      if (err) res.send(err);
+      res.json(result);
+    });
+  });
+};
 
 // 유저 정보 수정
-exports.update = function(req,res){
-    users.update(req.body, function(err,result){
-        if(err) res.send(err)
-        res.json(result)
-    })
-}
+exports.update = function (req, res) {
+  users.update(req.body, function (err, result) {
+    if (err) res.send(err);
+    res.json(result);
+  });
+};
 
 // 유저 데이터 삭제
-exports.delete = function(req,res){
-    users.delete(req.params.userID, function(err,result){
-        if(err) res.send(err)
-        res.json(result)
-    })
-}
+exports.delete = function (req, res) {
+  users.delete(req.params.userID, function (err, result) {
+    if (err) res.send(err);
+    res.json(result);
+  });
+};
--- appAPI/routes/users.js
+++ appAPI/routes/users.js
@@ -1,12 +1,26 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const users = require("../controller/users.controller")
+const users = require("../controller/users.controller");
+const { auth } = require("../middleware/authmiddleware");
 
 // 도메인/api/users/ 주소
-router.post('/auth',users.login)
-router.get('/:userID', users.findByID);
-router.post('/register',users.create)
-router.put('/modify',users.update)
-router.delete('/delete/:userID',users.delete)
+router.post("/login", users.login);
+router.get("/:userID", users.findByID);
+router.post("/register", users.create);
+router.put("/modify", users.update);
+router.delete("/delete/:userID", users.delete);
+
+// 토큰을 검증하는 엔드포인트
+router.get("/auth/payload", auth, (req, res) => {
+  const username = req.decoded.username;
+  console.log(req.decoded);
+  return res.status(200).json({
+    code: 200,
+    message: "토큰이 정상입니다.",
+    data: {
+      username: username,
+    },
+  });
+});
 
 module.exports = router;
